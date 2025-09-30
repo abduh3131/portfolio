a16e2607c17ae26aab0c59d717caac08b0425d2d
abduh3131 <94156984+abduh3131@users.noreply.github.com>
2025-09-30 18:53:30 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT: base:'./' so assets load correctly on GitHub Pages
+// IMPORTANT: base:'./' so assets resolve on GitHub Pages
 export default defineConfig({
   plugins: [react()],
   base: './',
